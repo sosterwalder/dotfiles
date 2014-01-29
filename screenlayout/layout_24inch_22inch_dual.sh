20ad66690bb11d968d782ea770579b412bc6cfc9
Sven Osterwalder <sven.osterwalder@gmail.com>
2014-01-29 23:22:49 +0100

    Adding of primary to one screen

--- screenlayout/layout_24inch_22inch_dual.sh
+++ screenlayout/layout_24inch_22inch_dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-I-1 --off --output DVI-I-0 --off --output DVI-I-3 --mode 1680x1050 --pos 2024x0 --rotate normal --output TV-0 --off
+xrandr --output DVI-I-2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-I-1 --off --output DVI-I-0 --off --output DVI-I-3 --mode 1680x1050 --pos 1976x0 --rotate normal --output TV-0 --off
